a84f34a4866a20be2b5944355318527132779a61
honki <276086064@qq.com>
2021-01-18 14:12:11 +0800

    update

---
+++ net/gonet.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main()  {
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		go spider(url, ch)
+	}
+
+	for range os.Args[1:] {
+		fmt.Println(<-ch)
+	}
+
+	fmt.Printf("%.2fs used \n", time.Since(start).Seconds())
+}
+
+// 子协程
+func spider(url string, ch chan<- string)  {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err)
+		return
+	}
+
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		ch <- fmt.Sprint(err)
+		return
+	}
+
+	usedTime := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs %d %s", usedTime, nbytes, url)
+}
