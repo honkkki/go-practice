ab58054f320777f37f537b448da92fcc5fae591a
honki <276086064@qq.com>
2021-02-01 23:14:00 +0800

    update

--- os/flag.go
+++ os/flag.go
@@ -4,11 +4,14 @@ import (
 	"flag"
 	"fmt"
 	"math/rand"
+	"strconv"
+	"strings"
 	"time"
 )
 
 var (
-	length int
+	length  int
+	charset string
 )
 
 func GetChars() string {
@@ -27,13 +30,21 @@ func GetChars() string {
 		A++
 	}
 
+	num := make([]string, 0, 10)
+
+	for i := 0; i < 10; i++ {
+		num = append(num, strconv.Itoa(i))
+	}
+
 	s := append(s1, s2...)
-	return string(s)
+	fmt.Println(num)
+	return string(s) + strings.Join(num, "")
 }
 
 // 获取命令行参数
-func getArgs()  {
+func getArgs() {
 	flag.IntVar(&length, "l", 8, "length of password")
+	flag.StringVar(&charset, "c", "char", "charset of password")
 	flag.Parse()
 }
 
--- slice/slice2.go
+++ slice/slice2.go
@@ -4,12 +4,16 @@ import (
 	"fmt"
 )
 
-func main()  {
+func main() {
 	sli := make([]string, 0, 10)
 	sli = append(sli, "hello")
 	sli = append(sli, "hello")
 	sli = append(sli, "hello")
 	fmt.Println(sli, len(sli), cap(sli))
 
+	var s1 []int
+	fmt.Println(s1 == nil)
+	s1 = append(s1, 1)
+	fmt.Println(s1)
 
 }
