7264f6d32f35c794fca11cb57bdc7b24dd0e65cf
honki <276086064@qq.com>
2021-03-14 01:35:11 +0800

    update

--- io/input.go
+++ io/input.go
@@ -7,13 +7,17 @@ func main() {
 	var a int
 	var b string
 	//var c float64
-	//_, _ = fmt.Scanf("%d%s", &a, &b)
+	_, _ = fmt.Scanf("%d%s", &a, &b)
+	fmt.Println(a, b)
 	//fmt.Scanf("%d", &a)
 	//fmt.Scanf("%s", &b)
 	//fmt.Scanf("%f", &c)
 	//fmt.Println(a, b, c)
 
-	fmt.Scan(&a, &b) // 回车换行分隔输入
+	_, err := fmt.Scan(&a, &b) // 回车换行或空格分隔输入
+	if err != nil {
+		fmt.Println(err)
+	}
 	fmt.Println(a, b)
 
 	fmt.Scanln(&a, &b) // 空格分隔输入 遇到回车退出
