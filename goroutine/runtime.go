627cf2e42f9483fda78b0125d0a0131c3fd0892c
honki <276086064@qq.com>
2021-04-29 00:22:55 +0800

    web

--- goroutine/runtime.go
+++ goroutine/runtime.go
@@ -9,5 +9,4 @@ func main()  {
 	// 查看系统cpu数目 go默认使用全部cpu数
 	fmt.Println(runtime.NumCPU())
 
-
 }
--- net/web.go
+++ net/web.go
@@ -7,10 +7,17 @@ import (
 )
 
 func main()  {
+	// 配置路由与处理逻辑
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/hello", hello)
 	log.Fatal(http.ListenAndServe("127.0.0.1:8080", nil))
+}
+
+func hello(w http.ResponseWriter, r *http.Request)  {
+	fmt.Fprintf(w, "hello golang http")
 }
 
 func handler(w http.ResponseWriter, r *http.Request)  {
 	fmt.Fprintf(w, "path = %q \n", r.URL.Path)
+
 }
