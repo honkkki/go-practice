53ddd2e97c6c10d88430d2770ee858a840a7d8db
honki <276086064@qq.com>
2021-05-17 23:06:26 +0800

    update

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.0 // indirect
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/golang/protobuf v1.5.2
+	github.com/jmoiron/sqlx v1.3.4 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/urfave/cli/v2 v2.3.0
 )
---
+++ sql/sqlx.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+	"log"
+)
+
+var sqlxDB *sqlx.DB
+
+type UserInfo struct {
+	Id        int64  `db:"id"`
+	Name      sql.NullString `db:"name"`
+	Age       int    `db:"age"`
+	CreatedAt sql.NullString `db:"created_at"`
+}
+
+func initSqlxDb() (err error) {
+	dsn := "root:111111@tcp(127.0.0.1:3306)/golang"
+	sqlxDB, err = sqlx.Open("mysql", dsn)
+	if err != nil {
+		return err
+	}
+
+	sqlxDB.SetMaxOpenConns(100)
+	sqlxDB.SetMaxIdleConns(15)
+	return nil
+}
+
+func queryData() {
+	var user UserInfo
+	sqlStr := "select name from user where id = ?"
+	err := sqlxDB.Get(&user, sqlStr, 1)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(user.Name.String)
+}
+
+func main() {
+	err := initSqlxDb()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	queryData()
+}
