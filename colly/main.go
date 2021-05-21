b19b63664a84b8045b586ab42424c1354086395f
honki <276086064@qq.com>
2021-05-21 12:14:08 +0800

    colly爬虫

--- colly/main.go
+++ colly/main.go
@@ -3,16 +3,41 @@ package main
 import (
 	"fmt"
 	"github.com/gocolly/colly"
+	"os"
+	"strings"
 )
 
+// 抓取豆瓣top250电影列表
 func main() {
 	c := colly.NewCollector(
+		// 按排序爬虫所以关闭异步爬虫方式
+		//colly.Async(true),
 		colly.UserAgent("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"),
 	)
+
+	c.Limits([]*colly.LimitRule{
+		{
+			DomainGlob: "*.douban.*",
+			Parallelism: 5,
+		},
+	})
+
+	c.OnHTML(".hd", func(e *colly.HTMLElement) {
+		fmt.Println(strings.Split(e.ChildAttr("a", "href"), "/")[4],
+			strings.TrimSpace(e.DOM.Find("span.title").Eq(0).Text()))
+
+			file, _ := os.OpenFile("./douban.txt", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0755)
+			defer file.Close()
+			str := fmt.Sprintf("id: %s    name: %s\n", strings.Split(e.ChildAttr("a", "href"), "/")[4],
+				strings.TrimSpace(e.DOM.Find("span.title").Eq(0).Text()))
+
+			file.WriteString(str)
+	})
 
 	c.OnHTML(".paginator a", func(e *colly.HTMLElement) {
 		e.Request.Visit(e.Attr("href"))
 	})
+
 	c.OnError(func(_ *colly.Response, err error) {
 		fmt.Println("Something went wrong:", err)
 	})
@@ -20,9 +45,7 @@ func main() {
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL)
 	})
-	c.OnScraped(func(response *colly.Response) {
-		fmt.Println(response.Request.URL)
-	})
-	c.Visit("https://movie.douban.com/top250?start=0&filter=")
 
+	c.Visit("https://movie.douban.com/top250?start=0&filter=")
+	c.Wait()
 }
