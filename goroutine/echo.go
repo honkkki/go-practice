c7e1e4262a349e7eed9715d2510b3ca115582bdb
honki <honkkki@yahoo.com>
2021-06-28 18:28:04 +0800

    update

---
+++ goroutine/echo.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"time"
+)
+
+func echo(c net.Conn, shout string, delay time.Duration) {
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+}
+
+func handleConnEcho(c net.Conn) {
+	input := bufio.NewScanner(c)
+	for input.Scan() {
+		echo(c, input.Text(), 1*time.Second)
+	}
+	c.Close()
+}
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:9000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println(err) // e.g., connection aborted
+			continue
+		}
+
+		handleConnEcho(conn)
+	}
+}
