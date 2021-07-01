5d5932c5c9f4fec54d405e01ca837fc0eb688f1a
honki <honkkki@yahoo.com>
2021-07-01 18:15:24 +0800

    update

---
+++ code/switch.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+func main() {
+	a := 1
+	switch {
+	case a == 0:
+		fmt.Println("a=0")
+	case a == 1:
+		fmt.Println("a=1")
+	}
+
+	switch aa:=100; {
+	case aa==1:
+		fmt.Println("aa=1")
+	case aa==100:
+		fmt.Println("aa=100")
+	}
+
+	fmt.Println(unsafe.Sizeof(a))
+
+	var num int32 = 1
+	str := "hello"
+	fmt.Println(unsafe.Sizeof(num))
+	fmt.Println(unsafe.Sizeof(str))
+
+
+
+}
