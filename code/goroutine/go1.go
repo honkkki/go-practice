6c53153bfb7ad78057047933c5985ff36178b1a5
honki <276086064@qq.com>
2021-07-16 00:26:12 +0800

    update

---
+++ code/goroutine/go1.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Ball struct {
+	hits int
+}
+
+func play(name string, table chan *Ball) {
+	for {
+		ball := <-table
+		ball.hits++
+		fmt.Println(name, ball.hits)
+		time.Sleep(100 * time.Millisecond)
+		table <- ball
+	}
+
+}
+
+func main() {
+	table := make(chan *Ball)
+	go play("pong", table)
+	go play("ping", table)
+	table<-new(Ball)
+	time.Sleep(3*time.Second)
+	<-table
+	fmt.Println("finish")
+}
---
+++ code/syncpool/pool.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type Logger struct {
+	time string
+	content string
+}
+
+var sp = sync.Pool{
+	New: func() interface{} {
+		return new(Logger)
+	},
+}
+
+
+func main() {
+	cached := sp.Get().(*Logger)
+	cached.time = time.Now().Format("2006-01-02 15:04:05")
+	cached.content = "first content"
+	fmt.Printf("%#v\n", cached)
+
+	sp.Put(cached)
+	cached2 := sp.Get().(*Logger)
+	fmt.Printf("%#v\n", cached2)
+
+	cached.time = ""
+	cached.content = ""
+	sp.Put(cached)
+	cached3 := sp.Get().(*Logger)
+	fmt.Printf("%#v\n", cached3)
+
+}
