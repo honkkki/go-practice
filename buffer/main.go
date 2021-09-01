d7e910ace1fc68c1a52f8f12e42761b179d755cc
honki <276086064@qq.com>
2021-09-01 18:56:02 +0800

    buffer

---
+++ buffer/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"net/url"
+)
+
+func main() {
+	buf := new(bytes.Buffer)
+	buf.WriteString("hello golang")
+	fmt.Println(buf.String())
+	str := "https://www.google.com/"
+	u, _ := url.Parse(str)
+	fmt.Println(u.Host)
+	fmt.Println(u.Path)
+}
